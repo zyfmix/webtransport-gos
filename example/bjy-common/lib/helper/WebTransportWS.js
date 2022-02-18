346ba72b38732d19dd854c1b79d6dcbe821ed1c9
zhaogaoxing <zhaogaoxing@baijiashilian.com>
2022-02-18 18:47:58 +0800

    feat: 支持多流同时收发

--- example/bjy-common/lib/helper/WebTransportWS.js
+++ example/bjy-common/lib/helper/WebTransportWS.js
@@ -6,17 +6,62 @@ import NetString from './NetString.js';
 import * as logger from '../util/logger.js';
 import execute from '../function/execute.js';
 import nextTick from '../function/nextTick.js';
+import * as array from '../util/array.js';
+import * as object from '../util/object.js';
+import getTimestamp from '../function/getTimestamp.js';
+const defaultOptions = {
+    enableCommand: false,
+    textCommand: 2,
+    binaryCommand: 1,
+    useDatagrams: false,
+    streamCount: 1,
+    textCommandStart: 2,
+    textCommandEnd: Math.pow(2, 31) - 1,
+    commandTimeout: 30 * 1000
+};
 export default class WebTransportWS {
     constructor(url, options) {
         this.url = url;
-        this.options = options;
+        this.options = object.extend({}, defaultOptions, options);
+        this.textCommandPointer = this.options.textCommandStart;
+        this.commandMap = new Map();
+        this.commandQueue = [];
+        this.streams = [];
+        this.readers = [];
+        this.writers = [];
         this.netString = new NetString({
             bufferSize: 1000 * 1024 * 1024,
-            onDecodeText: ((message) => {
+            onDecodeText: ((message, cmd) => {
                 if (this.onmessage) {
-                    execute(this.onmessage, this, [{
-                            data: message
-                        }]);
+
+                    if (this.options.streamCount > 1) {
+                        if (this.commandMap.get(cmd)) {
+                            // 已经收到过
+                            return;
+                        }
+                        execute(this.onmessage, this, [{
+                                data: message
+                            }]);
+                        this.commandMap.set(cmd, true);
+                        this.commandQueue.push({
+                            cmd,
+                            timestamp: getTimestamp()
+                        });
+                        while (this.commandQueue.length) {
+                            if (getTimestamp() - this.commandQueue[0].timestamp > this.options.commandTimeout) {
+                                this.commandMap.delete(this.commandQueue[0].cmd);
+                                this.commandQueue.shift();
+                            }
+                            else {
+                                break;
+                            }
+                        }
+                    }
+                    else {
+                        execute(this.onmessage, this, [{
+                                data: message
+                            }]);
+                    }
                 }
             }),
             enableCommand: this.options.enableCommand
@@ -43,37 +88,60 @@ export default class WebTransportWS {
         });
         this.connect();
     }
-    async read() {
+    async read(reader) {
         while (true) {
-            const { value, done } = await this.reader.read();
+            const { value, done } = await reader.read();
             if (done) {
                 break;
             }
             this.netString.decode(value);
         }
     }
+    handleRead() {
+        array.each(this.readers, reader => {
+            this.read(reader);
+        });
+    }
     async connect() {
         await this.transport.ready;
         if (this.options.useDatagrams) {
-            this.reader = this.transport.datagrams.readable.getReader();
-            this.writer = this.transport.datagrams.writable.getWriter();
+            this.readers.push(this.transport.datagrams.readable.getReader());
+            this.writers.push(this.transport.datagrams.writable.getWriter());
         }
         else {
-            this.stream = await this.transport.createBidirectionalStream();
-            this.reader = this.stream.readable.getReader();
-            this.writer = this.stream.writable.getWriter();
+            for (let i = 0; i < this.options.streamCount; i++) {
+                const stream = await this.transport.createBidirectionalStream();
+                const reader = stream.readable.getReader();
+                const writer = stream.writable.getWriter();
+                this.streams.push(stream);
+                this.readers.push(reader);
+                this.writers.push(writer);
+            }
         }
         nextTick(() => {
             if (this.onopen) {
                 execute(this.onopen, this, [{}]);
             }
         });
-        this.read();
+        this.handleRead();
+    }
+    getTextCommand() {
+        if (this.options.streamCount > 1) {
+            const command = this.textCommandPointer++;
+            if (this.textCommandPointer > this.options.textCommandEnd) {
+                this.textCommandPointer = this.options.textCommandStart;
+            }
+            return command;
+        }
+        return this.options.textCommand;
     }
     send(data) {
         const message = is.string(data) ? data : JSON.stringify(data);
         try {
-            this.writer.write(this.netString.encode(message, this.options.textCommand));
+            const cmd = this.getTextCommand()
+            array.each(this.writers, writer => {
+                writer.write(this.netString.encode(message, cmd));
+            });
         }
         catch (error) {
             logger.error(`send data error, ${error}`);
@@ -83,26 +151,24 @@ export default class WebTransportWS {
         }
     }
     sendBinary(buffer) {
-        this.writer.write(this.netString.encode(buffer, this.options.binaryCommand));
+        array.each(this.writers, writer => {
+            writer.write(this.netString.encode(buffer, this.options.binaryCommand));
+        });
     }
     close() {
         if (this.transport) {
             this.transport.close();
             this.transport = null;
         }
-        if (this.stream) {
-            this.stream = null;
-        }
-        if (this.reader) {
-            this.reader = null;
-        }
-        if (this.writer) {
-            this.writer = null;
-        }
         if (this.netString) {
             this.netString.destroy();
             this.netString = null;
         }
+        this.streams = null;
+        this.readers = null;
+        this.writers = null;
+        this.commandMap = null;
+        this.commandQueue = null;
     }
     clear() {
         this.close();
